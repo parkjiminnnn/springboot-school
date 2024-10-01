7fe0caee98f64e2be7186af44f4d5ef06944b11d
박지민 <pjm5489@naver.com>
2024-10-02 00:38:52 +0900

    feat: for문으로 표 출력기능 추가

--- src/main/java/inhatc/cse/jiminshop/item/controller/ItemController.java
+++ src/main/java/inhatc/cse/jiminshop/item/controller/ItemController.java
@@ -5,49 +5,71 @@ import inhatc.cse.jiminshop.item.dto.ItemDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class ItemController {
-
     @GetMapping("/item/thymeleaf1")
     public String thymeleaf1(Model model) {
-//        long id = 1L;
-//        String isOdd = "";
-//        for (int i = 1; i < 10; i++) {
-//            id = i;
-//            if (i % 2 == 0) {
-//                isOdd = "짝수";
-//            } else {
-//                isOdd = "홀수";
-//            }
-//        }
         ItemDto itemDto = ItemDto.builder()
                 .id(1L)
-                .itemNm("상품명")
-                .itemDetail("상품 상세 설명")
+                .itemNm("상품명123")
+                .itemDetail("상품상세설명123")
                 .price(10000)
                 .stockNumber(100)
-//                .isOdd(isOdd)
                 .build();
-//        List<ItemDto> list = new ArrayList<>() {};
-//        list.add(itemDto);
 
         model.addAttribute("item", itemDto);
-        return "item/thymeleaf1";
+        return "item/thymeleaf1";   //view단으로 갈때는 기본적으로 루트가 붙어서 /안해줘도 됨.
     }
 
     @GetMapping("/item/thymeleaf2")
-    public String thymeleaf2(ItemDataDto itemDataDto,
-                             Model model
-    ) {
-        System.out.println("========: " + itemDataDto);
+    public String thymeleaf2(ItemDataDto itemDataDto, Model model) {
+        System.out.println("==================이름" + itemDataDto);
         model.addAttribute("item", itemDataDto);
 
-
         return "item/thymeleaf2";
+    }
+
+    @GetMapping("/temp/temp")
+    public String ex() {
+
+        return "temp/temp";
+    }
+
+    @GetMapping("/item/thymeleaf3")
+    public String thymeleaf3(Model model) {
+        List<ItemDto> itemDtoList = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품상세설명" + i);
+            itemDto.setItemNm("테스트상품 " + i);
+            itemDto.setPrice(1000 * i);
+            itemDto.setStockNumber(10 * i);
+
+            itemDtoList.add(itemDto);
+
+        }
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "item/thymeleaf3";
+    }
+
+    @GetMapping("/item/thymeleaf4")
+    public String thymeleaf4(Model model) {
+        List<ItemDto> itemDtoList = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품상세설명" + i);
+            itemDto.setItemNm("테스트상품 " + i);
+            itemDto.setPrice(1000 * i);
+            itemDto.setStockNumber(10 * i);
+
+            itemDtoList.add(itemDto);
+
+        }
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "item/thymeleaf4";
     }
 }
--- src/main/java/inhatc/cse/jiminshop/item/dto/ItemDto.java
+++ src/main/java/inhatc/cse/jiminshop/item/dto/ItemDto.java
@@ -5,6 +5,7 @@ import lombok.*;
 @Setter
 @Getter
 @ToString
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class ItemDto {
